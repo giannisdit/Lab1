ce64ade549cea1ed071558a80321afff2c483b5d
Γίαννης <@LAPTOP-GFRG3E5P>
2020-03-23 16:53:35 +0200

    Written code to evaluate arithmetic expression

--- Lab11/Source.asm
+++ Lab11/Source.asm
@@ -6,12 +6,19 @@ Include Irvine32.inc
 	y SDWORD 154d
 	z SDWORD -17d
 	w SDWORD ?
-	
-
 .code
 main PROC
-
-
+	;w=  (x+ y) –(50d+ x+ y+ z-p)
+	MOV eax,x	;EAX=x
+	ADD eax,y	;EAX=x+y
+	
+	MOV ebx,50d	;EBX=50
+	ADD ebx,eax	;EBX=50+x+y
+	ADD ebx,z	;EBX=50+x+y+z
+	SUB ebx,p	;EBX=50+x+y-p
+	SUB eax,ebx ;EAX=(x+ y) –(50d+ x+ y+ z-p)
+	MOV w,eax	;w=EAX
+	call WriteInt
 	exit
 main ENDP
 END main
