208d3d28d8baf3c44aa73ade646a1eeead9c42cf
Γίαννης <@LAPTOP-GFRG3E5P>
2020-03-23 16:34:01 +0200

    Declere variables and symnolics constants

--- Lab11/Source.asm
+++ Lab11/Source.asm
@@ -1,7 +1,12 @@
 title Laboratory 1 : Evaluating Simple Arithmetic Expressions
 Include Irvine32.inc
-
+	p EQU 8d
 .data
+	x SDWORD 16d
+	y SDWORD 154d
+	z SDWORD -17d
+	w SDWORD ?
+	
 
 .code
 main PROC
